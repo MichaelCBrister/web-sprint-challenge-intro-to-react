946ee6fda8e05cf1dbccc7963771f40a1ad98926
MichaelCBrister <70389996+MichaelCBrister@users.noreply.github.com>
2020-10-09 11:16:31 -0500

    completed

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+
+import React from "react"
+import styled from "styled-components"
+const StyledCharacter = styled.div`
+.names {
+}
+h2{
+    color: white;
+    border: 6px solid white;
+    background-color: black;
+}`
+const  Character = (props) => {
+    const {name} = props
+    return(
+        <StyledCharacter className = "Character">
+            <div className = "names">
+                <h2>{name}</h2>
+            </div>
+        </StyledCharacter>
+    )
+}
+export default Character
